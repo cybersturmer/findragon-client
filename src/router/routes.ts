01630dbf6ffa660a32c133c81b5eaf11498b95a8
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-21 01:37:18 +0300

    Routes updated.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,9 +6,6 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/Index.vue') }],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/Error404.vue'),
