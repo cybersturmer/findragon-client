a6b86c2cd4975756e3d0c3cf4a6ec66c5bd70a9c
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-23 00:53:54 +0300

    Allocations page.

---
+++ src/pages/Allocations.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page padding>
+    <allocation-table
+      :row="row"
+    />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import AllocationTable from 'components/dash/allocation/AllocationTable.vue'
+
+const row = [
+  {
+    title: 'Indexed',
+    portfolio_ratio: 80
+  },
+  {
+    title: 'Bonds',
+    portfolio_ratio: 10
+  },
+  {
+    title: 'Shares',
+    portfolio_ratio: 10
+  }
+]
+
+export default defineComponent({
+  name: 'Allocations',
+  components: {
+    AllocationTable
+  },
+  setup () {
+    return {
+      row
+    }
+  }
+})
+</script>
