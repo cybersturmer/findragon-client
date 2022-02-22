62db1dfbdf4ab483b5247536d1696cd6da7176b2
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-23 00:11:54 +0300

    Allocation route.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,16 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/DashLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    children: [
+      {
+        path: '',
+        component: () => import('pages/Index.vue')
+      },
+      {
+        path: 'allocation',
+        component: () => import('pages/Allocations.vue')
+      }
+    ],
   },
   {
     path: '/:catchAll(.*)*',
