f7408fc20ffc5da2e326a6b66b13e1f84f6e7690
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-23 00:07:04 +0300

    Allocation table first simplified version.

---
+++ src/components/dash/allocation/AllocationTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-table
+    title="Allocation table"
+    :columns="columns"
+    :rows="row"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const columns = [
+  {
+    name: 'title',
+    required: true,
+    label: 'Title',
+    align: 'left',
+    field: (row: { title: string }) => row.title,
+    format: (val: string) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'portfolio_ratio',
+    required: true,
+    label: 'Ratio',
+    align: 'center',
+    field: (row : { portfolio_ratio: number }) => row.portfolio_ratio,
+    format: (val: string) => `${val}`,
+    sortable: true
+  }
+]
+
+export default defineComponent({
+  name: 'AllocationTable',
+  props: {
+    row: Array
+  },
+  setup () {
+    return {
+      columns
+    }
+  }
+})
+</script>
+
+
