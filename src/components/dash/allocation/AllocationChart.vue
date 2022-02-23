bc2724a05a06c9e79c352cb3195b8a5ff1975a2f
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-24 02:59:09 +0300

    Moved to separate component.

--- src/components/dash/allocation/AllocationChart.vue
+++ src/components/dash/allocation/AllocationChart.vue
@@ -1,10 +1,72 @@
 <template>
-  <div>My component</div>
+  <div>
+    <DoughnutChart v-bind="doughnutChartProps" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
+import { DoughnutChart, useDoughnutChart } from 'vue-chart-3'
+import { Chart, ChartData, ChartOptions, registerables } from 'chart.js'
+import { Allocation } from 'components/models'
+
+Chart.register(...registerables)
+
+const backgroundColor = [
+  '#77CEFF',
+  '#0079AF',
+  '#123E6B'
+]
+
 export default defineComponent({
-  name: 'AllocationChart'
+  name: 'AllocationChart',
+  components: { DoughnutChart },
+  props: {
+    row: Array as PropType<Allocation[]>,
+  },
+  setup (props) {
+    const titles = props.row?.map(obj => obj.title)
+    const values = props.row?.map(obj => obj.portfolio_ratio)
+
+    if (!titles || !values) throw new Error('Title and value cannot be undefined')
+
+    const dataObject = computed<ChartData<'doughnut'>>(() => ({
+      labels: titles,
+      datasets: [
+        {
+          data: values,
+          backgroundColor
+        },
+      ],
+    }))
+
+    const options = computed<ChartOptions<'doughnut'>>(() => ({
+      scales: {
+        myScale: {
+          display: false,
+          type: 'logarithmic',
+          position: 'left',
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+          position: 'bottom',
+        }
+      },
+    }))
+
+    const { doughnutChartProps, doughnutChartRef } = useDoughnutChart({
+      chartData: dataObject,
+      options,
+    })
+
+    return {
+      dataObject,
+      options,
+      doughnutChartProps,
+      doughnutChartRef
+    }
+  }
 })
 </script>
