69c97e28d8e9a1e0d3999bab4a831ad486cdda75
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-26 20:32:21 +0300

    Renamed variables.

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-item clickable tag="a" target="_self" :to="{ name: link }">
+  <q-item clickable tag="a" target="_self" :to="{ name: route }">
     <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
@@ -29,7 +29,7 @@ export default defineComponent({
       default: '',
     },
 
-    link: {
+    route: {
       type: String,
       default: '#',
     },
