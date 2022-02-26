951b7126855c9455933e742e06961678225944bb
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-26 18:41:11 +0300

    Now Essential link allow us to use named routes.

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-item clickable tag="a" target="_blank" :href="link">
+  <q-item clickable tag="a" target="_self" :to="{ name: link }">
     <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
 
     <q-item-section>
       <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>
+      <q-item-label caption class="text-grey-1">
         {{ caption }}
       </q-item-label>
     </q-item-section>
