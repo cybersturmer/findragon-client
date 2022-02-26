2095ca095884ecdfb616a1a90a5e801da15841e6
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-26 20:05:31 +0300

    Assets page works finally.

--- src/pages/Assets.vue
+++ src/pages/Assets.vue
@@ -1,14 +1,45 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div v-if="isAssetDataAvailable"
+         class="row">
+      <div class="col">
+        <assets-table :row="assets" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { getCurrentInstance, onMounted, computed, ref } from 'vue'
+import AssetsTable from 'components/dash/assets/AssetsTable.vue'
+
 export default {
   name: 'Assets',
+  components: {AssetsTable},
   setup () {
+    const assets = ref(null)
+    const $vueInstance = getCurrentInstance()
 
+    const { $api } = $vueInstance.appContext.config.globalProperties
+
+    onMounted(async () => {
+      try {
+        const response = await $api.get('/assets/')
+
+        assets.value = response.data
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    const isAssetDataAvailable = computed(() => {
+      return Array.isArray(assets.value) && assets.value.length > 0
+    })
+
+    return {
+      assets,
+      isAssetDataAvailable
+    }
   }
 }
 </script>
