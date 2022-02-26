5b48f9b36c16f8b564afedcb5099cf51359d2296
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-26 19:23:08 +0300

    Assets table initial version.

---
+++ src/components/dash/assets/AssetsTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-table
+    title="Assets table"
+    :columns="columns"
+    :rows="row"
+  />
+</template>
+
+<script>
+
+const columns = [
+  {
+    name: 'title',
+    required: true,
+    label: 'Title',
+    align: 'left',
+    field: (row) => row.description,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'ticker',
+    required: true,
+    label: '',
+    align: 'center',
+    field: (row) => row.ticker,
+    format: (val) => `${val}`
+  }
+]
+
+export default {
+  name: 'AssetsTable',
+  props: {
+    row: Array
+  },
+  setup () {
+    return {
+      columns
+    }
+  }
+}
+</script>
