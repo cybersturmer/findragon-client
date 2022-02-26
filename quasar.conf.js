05e4595eac3ac8a9cc35661326434f6243eb117a
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-02-26 21:42:23 +0300

    We will need this plugin soon.

--- quasar.conf.js
+++ quasar.conf.js
@@ -95,7 +95,9 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: [
+        'Meta'
+      ],
     },
 
     // animations: 'all', // --- includes all animations
