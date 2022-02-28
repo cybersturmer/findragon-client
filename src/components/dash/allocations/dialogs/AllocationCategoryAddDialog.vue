df5636a9242e405103b7c7e25b4be40ab09d6948
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-01 00:07:10 +0300

    Templates for Asset / Category add modals.

--- src/components/dash/allocations/dialogs/AllocationCategoryAddDialog.vue
+++ src/components/dash/allocations/dialogs/AllocationCategoryAddDialog.vue
@@ -1,19 +1,5 @@
 <template>
-  <!-- notice dialogRef here -->
-  <q-dialog ref="dialogRef" @hide="onDialogHide">
-    <q-card class="q-dialog-plugin">
-      <!--
-        ...content
-        ... use q-card-section for it?
-      -->
-
-      <!-- buttons example -->
-      <q-card-actions align="right">
-        <q-btn color="primary" label="OK" @click="onOKClick" />
-        <q-btn color="primary" label="Cancel" @click="onCancelClick" />
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
+  <div>My component</div>
 </template>
 
 <script>
