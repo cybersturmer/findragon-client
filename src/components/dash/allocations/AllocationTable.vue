91ac3e336434a22ba584175a30ac4c381650c282
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-05 02:16:11 +0300

    Allocation table is able to be edited.

--- src/components/dash/allocations/AllocationTable.vue
+++ src/components/dash/allocations/AllocationTable.vue
@@ -1,18 +1,69 @@
 <template>
   <q-table
+    v-model:selected="selectedTableRows"
     flat
     bordered
-    hide-bottom
+    separator="cell"
+    :rows-per-page-options="[0]"
     :selection="selection"
     :columns="columns"
     :rows="row"
-  />
+  >
+    <template v-if="isEditing" #body-cell-title="props">
+        <q-td key="title" :props="props">
+          {{ props.value }}
+          <q-popup-edit v-model="props.value" v-slot="scope">
+            <q-input v-model="scope.value" dense autofocus counter />
+          </q-popup-edit>
+        </q-td>
+    </template>
+    <template v-if="isEditing" #body-cell-ratio="props">
+      <q-td key="ratio" :props="props">
+        {{ props.value }}
+        <q-popup-edit
+          v-model="props.value"
+          v-slot="scope"
+          :title="`${props.row.title} ratio`"
+          label-set="save"
+          square
+          buttons
+          auto-save
+        >
+          <q-input
+            v-model="scope.value"
+            color="dark"
+            dense
+            autofocus
+            counter
+            suffix="%"
+          />
+        </q-popup-edit>
+      </q-td>
+    </template>
+    <template v-if="isEditing" #bottom>
+      <q-btn-group outline class="q-ma-sm float-right">
+        <q-btn
+          color="negative"
+          label="Remove"
+          :disable="!areSelected"
+          @click="removeAllocations"
+        />
+        <q-btn
+          color="primary"
+          label="Complete"
+          @click="completeEditing"
+        />
+      </q-btn-group>
+    </template>
+  </q-table>
 </template>
 
 <script>
 import {
+  ref,
   computed,
-  defineComponent
+  defineComponent,
+  getCurrentInstance
 } from 'vue'
 
 const columns = [
@@ -26,11 +77,11 @@ const columns = [
     sortable: true
   },
   {
-    name: 'portfolio_ratio',
+    name: 'ratio',
     required: true,
     label: 'Ratio %',
     align: 'center',
-    field: (row) => row.portfolio_ratio / 100,
+    field: (row) => row.category_ratio / 100,
     format: (val) => `${val}`,
     sortable: true
   }
@@ -38,18 +89,53 @@ const columns = [
 
 export default defineComponent({
   name: 'AllocationTable',
+  emits: [
+    'completed'
+  ],
   props: {
     row: Array,
     isEditing: Boolean
   },
-  setup (props) {
+  setup (props, { emit }) {
     const selection = computed(() => {
       return props.isEditing ? 'multiple' : 'none'
     })
 
+    const selectedTableRows = ref([])
+
+    const areSelected = computed(() => {
+      return Array.isArray(selectedTableRows.value) && selectedTableRows.value.length > 0
+    })
+
+    const $vueInstance = getCurrentInstance()
+    const { $api } = $vueInstance.appContext.config.globalProperties
+
+    const removeAllocation = async (id) => {
+      try {
+        await $api.delete(`/allocations/${id}`)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    const removeAllocations = async () => {
+      for (const el of selectedTableRows.value) {
+        console.log(el)
+      }
+    }
+
+    const completeEditing = () => {
+      selectedTableRows.value = []
+      emit('completed')
+    }
+
     return {
       columns,
-      selection
+      selection,
+      areSelected,
+      selectedTableRows,
+      completeEditing,
+      removeAllocations
     }
   }
 })
