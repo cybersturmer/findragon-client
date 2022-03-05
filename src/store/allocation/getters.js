f60079eeba2e5ca81e1a4e133c04eb70eaaed939
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-06 01:59:07 +0300

    Some versions for allocations getters.

---
+++ src/store/allocation/getters.js
@@ -0,0 +1,14 @@
+export function ALLOCATIONS (state) {
+  return state.allocations
+}
+
+export function ALLOCATIONS_BY_PARENT (state, getters) {
+  return parentId => {
+    if (parentId == null) {
+      return getters.ALLOCATIONS
+    } else {
+      return state.allocations
+        .filter((allocation) => allocation.children)[0]
+    }
+  }
+}
