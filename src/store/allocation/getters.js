389969aac8aef4bf8b3ad8c58932f641ef75eb58
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-07 03:55:21 +0300

    Get allocations by id.

--- src/store/allocation/getters.js
+++ src/store/allocation/getters.js
@@ -9,7 +9,13 @@ export function ALLOCATIONS_BY_PARENT (state, getters) {
         .filter((allocation) => allocation.parent_id === null)
     } else {
       return state.allocations
-        .filter((allocation) => allocation.id === parentId)[0].children
+        .filter((allocation) => allocation.id === parentId)[0]?.children
     }
+  }
+}
+
+export function ALLOCATION_BY_ID (state) {
+  return allocationId => {
+    return state.allocations.filter((allocation) => allocation.id === allocationId)[0]
   }
 }
