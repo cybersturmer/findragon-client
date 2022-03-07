ea109e574222634e6e3731183cf509adb2d89bab
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-07 03:18:02 +0300

    Getters allocation by parent -> return all allocations by default.

--- src/store/allocation/getters.js
+++ src/store/allocation/getters.js
@@ -6,6 +6,7 @@ export function ALLOCATIONS_BY_PARENT (state, getters) {
   return parentId => {
     if (parentId == null) {
       return getters.ALLOCATIONS
+        .filter((allocation) => allocation.parent_id === null)
     } else {
       return state.allocations
         .filter((allocation) => allocation.id === parentId)[0].children
