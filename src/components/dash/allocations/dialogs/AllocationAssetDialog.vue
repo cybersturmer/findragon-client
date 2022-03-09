3fba18e75217d46ab1b5baa759653ec080cd387d
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-10 02:46:19 +0300

    ticker and exchange are not editables if we not create but edit allocation.

--- src/components/dash/allocations/dialogs/AllocationAssetDialog.vue
+++ src/components/dash/allocations/dialogs/AllocationAssetDialog.vue
@@ -2,12 +2,14 @@
   <q-dialog ref="dialogRef" @hide="onDialogHide">
     <q-card class="q-dialog-plugin q-px-md q-py-sm">
       <q-input
+        v-show="!isEditing"
         autofocus
         v-model="ticker"
         label="Ticker"
         type="text"
       />
       <q-input
+        v-show="!isEditing"
         v-model="exchange"
         label="Exchange"
         type="text"
@@ -30,7 +32,7 @@
 
 <script>
 import { useDialogPluginComponent } from 'quasar'
-import { getCurrentInstance, ref } from 'vue'
+import { computed, getCurrentInstance, ref } from 'vue'
 
 export default {
   name: 'AllocationAssetDialog',
@@ -57,16 +59,18 @@ export default {
     const $vueInstance = getCurrentInstance()
     const { $api, $store } = $vueInstance.appContext.config.globalProperties
 
+    const isEditing = computed(() => {
+      return props.editing
+    })
+
     const ticker = ref('')
     const exchange = ref('')
     const ratio = ref(1)
 
-    console.dir(props)
-
     // Let's predefine if it's editing.
     if (props.editing) {
-      ticker.value = props.node.asset.ticker
-      exchange.value = props.node.asset.exchange
+      ticker.value = props.node.ticker
+      exchange.value = props.node.exchange
       ratio.value = props.node.category_ratio / 100
     }
 
@@ -77,15 +81,17 @@ export default {
           title: `${ticker.value}.${exchange.value}`,
           portfolio_id: 1, // It should be defined from $store.
           category_ratio: parseInt(ratio.value) * 100,
-          parent_id: props.editing ? props.node.parent_id : props.parentNodeId,
-          ticker: ticker.value,
-          exchange: exchange.value
+          parent_id: props.editing ? props.node.parent_id : props.parentNodeId
         }
 
         if (props.editing) {
           const { data } = await $api.patch(`/allocations/${props.node.id}`, payload)
           $store.commit('allocation/UPDATE_ALLOCATION', data)
         } else {
+
+          payload.ticker = ticker.value
+          payload.exchange = exchange.value
+
           const { data } = await $api.post('/allocations/', payload)
           $store.commit('allocation/ADD_ALLOCATION', data)
         }
@@ -104,6 +110,7 @@ export default {
       exchange,
       ratio,
       dialogRef,
+      isEditing,
       onDialogHide,
       onOKClick
     }
