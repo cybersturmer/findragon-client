e681cd59b65dc2ee70e399cec6f12bc5846304a3
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-11 01:38:25 +0300

    Let's wrap it to scroll point and unlimited rows per page.

--- src/components/dash/assets/AssetsTable.vue
+++ src/components/dash/assets/AssetsTable.vue
@@ -1,11 +1,14 @@
 <template>
-  <q-table
-    flat
-    bordered
-    hide-bottom
-    :columns="columns"
-    :rows="row"
-  />
+  <q-scroll-area style="height: 90vh">
+    <q-table
+      flat
+      bordered
+      separator="horizontal"
+      :rows-per-page-options="[0]"
+      :columns="columns"
+      :rows="row"
+    />
+  </q-scroll-area>
 </template>
 
 <script>
