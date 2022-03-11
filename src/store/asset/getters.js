6435824232ac5f83245a70898f21841368b10a94
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-12 00:37:11 +0300

    Additional getters for asset.

--- src/store/asset/getters.js
+++ src/store/asset/getters.js
@@ -1,4 +1,9 @@
-/*
-export function someGetter (state) {
+export function ASSETS (state) {
+  return state.assets
 }
-*/
+
+export function ASSET_BY_ID (state) {
+  return assetId => {
+    return state.assets.filter((asset) => asset.id === assetId).pop()
+  }
+}
