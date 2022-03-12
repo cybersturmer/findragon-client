1e5bb81aef64d24d4c21fc8cec4787056ee49e4d
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-12 19:43:08 +0300

    Currencies enum added.

--- src/services/enums.js
+++ src/services/enums.js
@@ -16,3 +16,24 @@ export const assetTypesEnum = {
   BOND: 2,
   FUND: 3
 }
+
+export const currenciesEnum = {
+  USD: 'USD',
+  RUB: 'RUB',
+  EUR: 'EUR',
+  AUD: 'AUD',
+  CAD: 'CAD',
+  CHF: 'CHF',
+  CNY: 'CNY',
+  CZK: 'CZK',
+  GBP: 'GBP',
+  HKD: 'HKD',
+  ILS: 'ILS',
+  JPY: 'JPY',
+  KZT: 'KZT',
+  PLN: 'PLN',
+  SEK: 'SEK',
+  SGD: 'SGD',
+  TRY: 'TRY',
+  UAH: 'UAH',
+}
