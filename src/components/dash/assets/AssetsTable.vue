8950fd8ed284bacb3a3252b2839897bbff535bb4
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-14 00:02:22 +0300

    Assets table can show total and average price for selected asset.

--- src/components/dash/assets/AssetsTable.vue
+++ src/components/dash/assets/AssetsTable.vue
@@ -19,7 +19,7 @@ const columns = [
     required: true,
     label: 'Title',
     align: 'left',
-    field: (row) => row.description,
+    field: (row) => row.asset.description,
     format: (val) => `${val}`,
     sortable: true
   },
@@ -28,8 +28,35 @@ const columns = [
     required: true,
     label: 'Ticker',
     align: 'center',
-    field: (row) => row.ticker,
+    field: (row) => row.asset.ticker,
     format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'amount',
+    required: true,
+    label: 'Amount',
+    align: 'center',
+    field: (row) => row.amount,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'total_price',
+    required: true,
+    label: 'Total price',
+    align: 'center',
+    field: (row) => row.total_price,
+    format: (val) => `${ val ? val : '-' }`,
+    sortable: true
+  },
+  {
+    name: 'avg_price',
+    required: true,
+    label: 'Average price',
+    align: 'center',
+    field: (row) => row.avg_price,
+    format: (val) => `${ val ? val : '-' }`,
     sortable: true
   }
 ]
