3a2f0c5c325facd82cfe58eff62fd901a41da8fe
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-13 03:31:58 +0300

    Just base getter for transaction module.

--- src/store/transaction/getters.js
+++ src/store/transaction/getters.js
@@ -1,4 +1,9 @@
-/*
-export function someGetter (state) {
+export function TRANSACTIONS (state) {
+  return state.transactions
 }
-*/
+
+export function TRANSACTION_BY_ID (state) {
+  return transactionId => {
+    return state.transactions.filter((transaction) => transaction.id === transactionId).pop()
+  }
+}
