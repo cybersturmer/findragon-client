75a9078ef309a89ecd77ddb907705043a6ae3162
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-16 00:55:12 +0300

    Income getters.

--- src/store/income/getters.js
+++ src/store/income/getters.js
@@ -1,4 +1,9 @@
-/*
-export function someGetter (state) {
+export function INCOME (state) {
+  return state.income
 }
-*/
+
+export function INCOME_BY_ID (state) {
+  return incomeId => {
+    return state.income.filter((income) => income.id === incomeId).pop()
+  }
+}
