fa64755f94ed8ea9e7f8bfb6150308f5967055d3
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-17 23:33:20 +0300

    Initial mutations to manage income.

--- src/store/income/mutations.js
+++ src/store/income/mutations.js
@@ -1,4 +1,52 @@
-/*
-export function someMutation (state) {
+import {
+  removeElementById,
+  updateElementById,
+  StorageUtil
+} from 'src/services/utils'
+
+import { empty } from 'src/store/asset/presets'
+
+const storageUtil = new StorageUtil(
+  'income',
+  empty()
+)
+
+const saveIncomeState = (state) => {
+  storageUtil.setState('income', state.income)
 }
-*/
+
+export function ACTIVATE (state) {
+  state.enabled = true
+  storageUtil.setState('enabled', true)
+}
+
+export function SET_INCOME (state, payload) {
+  state.income = payload
+  saveIncomeState(state)
+}
+
+export function ADD_INCOME (state, payload) {
+  state.income.push(payload)
+  saveIncomeState(state)
+}
+
+export function UPDATE_INCOME (state, payload) {
+  updateElementById(state.income, payload)
+  saveIncomeState(state)
+}
+
+export function REMOVE_INCOME (state, payload) {
+  removeElementById(state.income, payload)
+  saveIncomeState(state)
+}
+
+export function REMOVE_INCOMES (state, payload) {
+  for (const element of payload) {
+    removeElementById(state.income, element)
+  }
+  saveIncomeState(state)
+}
+
+export function RESET (state) {
+  storageUtil.clearState(state)
+}
