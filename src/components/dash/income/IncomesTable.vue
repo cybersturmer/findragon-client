4d46d59b49aaa117c799180235f50c40382361b0
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-18 00:34:33 +0300

    Just columns defined.

--- src/components/dash/income/IncomesTable.vue
+++ src/components/dash/income/IncomesTable.vue
@@ -9,6 +9,65 @@ import {
   getCurrentInstance
 } from 'vue'
 
+import { date as quasarDate } from 'quasar'
+
+const columns = [
+  {
+    name: 'operation',
+    required: true,
+    label: 'Operation',
+    align: 'left',
+    field: (row) => row.operation,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'asset',
+    required: true,
+    label: 'Asset',
+    align: 'left',
+    field: (row) => row.asset.description,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'amount',
+    required: true,
+    label: 'Amount',
+    align: 'left',
+    field: (row) => row.amount,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'price',
+    required: true,
+    label: 'Price',
+    align: 'left',
+    field: (row) => row.price,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'tax',
+    required: true,
+    label: 'Tax',
+    align: 'left',
+    field: (row) => row.tax,
+    format: (val) => `${val}`,
+    sortable: true
+  },
+  {
+    name: 'date',
+    required: true,
+    label: 'Date',
+    align: 'left',
+    field: (row) => quasarDate.formatDate(row.date, 'DD.MM.YYYY'),
+    format: (val) => `${val}`,
+    sortable: true
+  }
+]
+
 export default {
   name: 'IncomesTable',
   props: {
@@ -28,8 +87,40 @@ export default {
       return Array.isArray(selectedTableRows.value) && selectedTableRows.value.length > 0
     })
 
-    return {
+    const removeIncome = async (id) => {
+      try {
+        await $api.delete(`/incomes/${id}`)
+      } catch (e) {
+        console.error(e)
+      }
+    }
 
+    const removeIncomes = async (id) => {
+      try {
+        for (const el of selectedTableRows.value) {
+          await removeIncome(el.id)
+        }
+
+        $store.commit('income/REMOVE_INCOMES', selectedTableRows.value)
+        selectedTableRows.value = []
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    const completeEditing = () => {
+      selectedTableRows.value = []
+      emit('completed')
+    }
+
+    return {
+      columns,
+      pagination,
+      selection,
+      areSelected,
+      completeEditing,
+      removeIncomes,
+      selectedTableRows
     }
   }
 }
