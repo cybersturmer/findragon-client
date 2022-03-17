264692316f6e7b40a0ffb48626cf490950a11bd3
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-17 23:51:06 +0300

    Component prototyping next step.

--- src/components/dash/income/IncomesTable.vue
+++ src/components/dash/income/IncomesTable.vue
@@ -3,13 +3,31 @@
 </template>
 
 <script>
+import {
+  ref,
+  computed,
+  getCurrentInstance
+} from 'vue'
+
 export default {
   name: 'IncomesTable',
   props: {
     incomes: Array,
     isEditing: Boolean
   },
-  setup () {
+  setup (props, { emit }) {
+    const $vueInstance = getCurrentInstance()
+    const { $api, $store } = $vueInstance.appContext.config.globalProperties
+
+    const selection = computed(() => {
+      return props.isEditing ? 'multiple' : 'none'
+    })
+
+    const selectedTableRows = ref([])
+    const areSelected = computed(() => {
+      return Array.isArray(selectedTableRows.value) && selectedTableRows.value.length > 0
+    })
+
     return {
 
     }
