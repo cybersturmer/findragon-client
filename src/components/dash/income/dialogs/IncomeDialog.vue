ae0b66b6a27579f7eca3097c9b408c85b3788783
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-19 03:37:51 +0300

    One more step to add / edit income dialog.

--- src/components/dash/income/dialogs/IncomeDialog.vue
+++ src/components/dash/income/dialogs/IncomeDialog.vue
@@ -1,25 +1,14 @@
 <template>
   <q-dialog ref="dialogRef" @hide="onDialogHide">
     <q-card class="q-dialog-plugin q-px-md q-py-sm">
-      <q-input
-        :disable="isEditing"
-        :autofocus="!isEditing"
-        v-model="ticker"
-        label="Ticker"
-        type="text"
-      />
-      <q-input
-        :disable="isEditing"
-        v-model="exchange"
-        label="Exchange"
-        type="text"
-      />
-      <q-input
-        v-model="ratio"
-        :autofocus="isEditing"
-        label="Ratio"
-        type="number"
-        @keydown.enter.prevent="onOKClick"
+      <!-- Select for assets -->
+      <q-select
+        v-model="incomeAsset"
+        :options="filteredAssetsOptions"
+        @filter="filterAssets"
+        label="Asset"
+        use-input
+        clearable
       />
       <q-card-actions vertical>
         <q-btn
@@ -34,8 +23,8 @@
 
 <script>
 import { useDialogPluginComponent, date as quasarDate } from 'quasar'
-import { operationTypesMapping } from 'src/services/enums'
-import { computed, getCurrentInstance, ref } from 'vue'
+import { incomeTypesMapping, currenciesOptions } from 'src/services/enums'
+import {computed, getCurrentInstance, onMounted, ref} from 'vue'
 
 const dateFormat = 'MM/DD/YYYY'
 
@@ -57,13 +46,50 @@ export default {
     const { $api, $store } = $vueInstance.appContext.config.globalProperties
 
     const todayString = quasarDate.formatDate(new Date(), dateFormat)
+    const assets = computed(() => $store.getters['asset/ASSETS'])
+    const assetsOptions = computed(() => {
+      return assets.value.map(option => ({ label: option.description, value: option.id }))
+    })
+
+    const filteredAssetsOptions = ref([])
+
+    const incomeType = ref(incomeTypesMapping[0])
+    const incomeAsset = ref(assetsOptions.value[0])
+    const incomeDate = ref(todayString)
+
+    const incomeCurrency = ref(currenciesOptions[0])
+    const incomeAmount = ref(null)
+    const incomePrice = ref(null)
+
+    const incomeTax = ref(null)
+
+    onMounted(async () => {
+      try {
+        const { data } = await $api.get('/assets/')
+
+        $store.commit('asset/SET_ASSETS', data)
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    const filterAssets = (val, update, abort) => {
+      if (val === '') {
+        update(() => {
+          filteredAssetsOptions.value = assetsOptions.value
+        })
+        return
+      }
+
+      update(() => {
+        const needle = val.toLowerCase()
+        filteredAssetsOptions.value = assetsOptions.value.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    }
 
     const isEditing = computed(() => {
       return props.editing
     })
-
-    const ticker = ref('')
-    const exchange = ref('')
 
     const saveIncome = async () => {
       try {
@@ -79,14 +105,34 @@ export default {
           ticker: "string",
           exchange: "string"
         }
+
+        const { data } = await $api.post('/incomes/', payload)
+
+        $store.commit('incomes/ADD_INCOME', data)
       } catch (e) {
         console.error(e)
       }
     }
 
+    const onOKClick = () => {
+      saveIncome()
+      onDialogOK()
+    }
+
     return {
       dialogRef,
-      onDialogHide
+      isEditing,
+      onDialogHide,
+      filterAssets,
+      onOKClick,
+      incomeType,
+      incomeAmount,
+      incomeAsset,
+      incomeCurrency,
+      incomeDate,
+      incomePrice,
+      incomeTax,
+      filteredAssetsOptions
     }
   }
 }
