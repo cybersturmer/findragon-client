a7d7c289fa711d5856aa11cf33a0040f6229e907
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-19 23:26:01 +0300

    Moved it to separate component

---
+++ src/components/dash/assets/AssetIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col col-auto q-pa-xs bg-grey-3" style="border-radius: 5px">
+    <q-img
+      height="30px"
+      width="30px"
+      fit="scale-down"
+      :src="`https://eodhistoricaldata.com/img/logos/${exchange}/${ticker}.png`"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetIcon',
+  props: {
+    ticker: {
+      type: String,
+      required: true
+    },
+     exchange: {
+      type: String,
+      required: true
+     }
+  },
+  setup () {
+    return {}
+  }
+}
+</script>
