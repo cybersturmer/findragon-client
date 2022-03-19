9dd44116e85457ca84a5f7e9bc83a9be2ae75b44
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-19 03:01:14 +0300

    One more step to have editable table of income.

--- src/components/dash/income/dialogs/IncomeDialog.vue
+++ src/components/dash/income/dialogs/IncomeDialog.vue
@@ -1,12 +1,53 @@
 <template>
-  <div>My component</div>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin q-px-md q-py-sm">
+      <q-input
+        :disable="isEditing"
+        :autofocus="!isEditing"
+        v-model="ticker"
+        label="Ticker"
+        type="text"
+      />
+      <q-input
+        :disable="isEditing"
+        v-model="exchange"
+        label="Exchange"
+        type="text"
+      />
+      <q-input
+        v-model="ratio"
+        :autofocus="isEditing"
+        label="Ratio"
+        type="number"
+        @keydown.enter.prevent="onOKClick"
+      />
+      <q-card-actions vertical>
+        <q-btn
+          flat
+          label="Save"
+          @click="onOKClick"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script>
+import { useDialogPluginComponent } from 'quasar'
+import { computed, getCurrentInstance, ref } from 'vue'
+
 export default {
-  // name: 'ComponentName',
+  name: 'IncomeDialog',
   setup () {
-    return {}
+    const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent()
+
+    const $vueInstance = getCurrentInstance()
+    const { $api, $store } = $vueInstance.appContext.config.globalProperties
+
+    return {
+      dialogRef,
+      onDialogHide
+    }
   }
 }
 </script>
