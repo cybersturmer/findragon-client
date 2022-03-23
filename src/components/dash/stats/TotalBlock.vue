51bf8d5e59a4ed629a5b4b4d45350d418a08bd60
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-24 01:41:01 +0300

    Block to show total purchase / price information on the stats page.

--- src/components/dash/stats/TotalBlock.vue
+++ src/components/dash/stats/TotalBlock.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>My component</div>
+  <q-card>
+    <q-card-section>
+      <div class="text-subtitle1 text-bold">
+        Total Price: {{totalPrice}}
+      </div>
+      <div class="text-subtitle2">
+        Total Purchase: {{totalPurchase}}
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
 export default {
-  // name: 'ComponentName',
+  name: 'TotalBlock',
+  props: {
+    totalPurchase: {
+      required: true
+    },
+    totalPrice: {
+      required: true
+    }
+  },
   setup () {
     return {}
   }
