6f82bad9b1a9f28cb2552ee1716affe8377d481b
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-24 02:27:31 +0300

    Income card finally looks better.

--- src/components/dash/stats/IncomeCard.vue
+++ src/components/dash/stats/IncomeCard.vue
@@ -1,17 +1,55 @@
 <template>
   <q-card>
     <q-card-section>
-      <div class="text-subtitle1 text-bold">
-        Total Income: {{totalIncome}}
+      <div class="row items-end">
+        <div class="col-auto">
+          <q-icon
+            :name="totalIncomeIconParams.icon"
+            :color="totalIncomeIconParams.color"
+            size="md"
+          />
+        </div>
+        <div class="col text-subtitle1 text-bold">
+          Total Income:
+          <span :class="`text-${totalIncomeIconParams.color}`">
+            {{totalIncomeIconParams.prefix}}{{totalIncome}}₽
+          </span>
+        </div>
       </div>
-      <div class="text-subtitle2">
-        Day Income: {{dayIncome}}
+      <div class="row items-center">
+        <div class="col-auto">
+          <q-icon
+            :name="dayIncomeIconParams.icon"
+            :color="dayIncomeIconParams.color"
+            size="md"
+          />
+        </div>
+        <div class="col text-subtitle2">
+          Day Income:
+          <span :class="`text-${dayIncomeIconParams.color}`">
+            {{dayIncomeIconParams.prefix}}{{dayIncome}}₽
+          </span>
+        </div>
       </div>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
+import { computed } from 'vue'
+
+const negativeIconAttrs = {
+  icon: 'arrow_drop_down',
+  color: 'negative',
+  prefix: ''
+}
+
+const positiveIconAttrs = {
+  icon: 'arrow_drop_up',
+  color: 'positive',
+  prefix: '+'
+}
+
 export default {
   name: 'IncomeCard',
   props: {
@@ -22,9 +60,18 @@ export default {
       required: true
     }
   },
-  setup () {
-    return {
+  setup (props) {
+    const dayIncomeIconParams = computed(() => {
+      return props.dayIncome > 0 ? positiveIconAttrs : negativeIconAttrs
+    })
 
+    const totalIncomeIconParams = computed(() => {
+      return props.totalIncome > 0 ? positiveIconAttrs : negativeIconAttrs
+    })
+
+    return {
+      dayIncomeIconParams,
+      totalIncomeIconParams
     }
   }
 }
