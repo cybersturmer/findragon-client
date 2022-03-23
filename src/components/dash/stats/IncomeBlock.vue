84bc8cbfe32e79b8feb3e3e97879963ff1b6bb47
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-24 01:40:17 +0300

    Block to show income information on the stats page.

--- src/components/dash/stats/IncomeBlock.vue
+++ src/components/dash/stats/IncomeBlock.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>My component</div>
+  <q-card>
+    <q-card-section>
+      <div class="text-subtitle1 text-bold">
+        Total Income: {{totalIncome}}
+      </div>
+      <div class="text-subtitle2">
+        Day Income: {{dayIncome}}
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
 export default {
-  // name: 'ComponentName',
+  name: 'IncomeBlock',
+  props: {
+    totalIncome: {
+      required: true
+    },
+    dayIncome: {
+      required: true
+    }
+  },
   setup () {
-    return {}
+    return {
+
+    }
   }
 }
 </script>
