66ef851e86379f31b4916432bae2b90440fdd3fd
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-23 23:36:59 +0300

    Additional Stats page to show main information for portfolio.

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,6 +1,31 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="row items-start q-gutter-md">
+      <div class="col">
+        <q-card>
+          <q-card-section>
+            <div>
+              Total Purchase
+            </div>
+            <div>
+              Total Market Price
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col">
+        <q-card>
+          <q-card-section>
+            <div>
+              Total income
+            </div>
+            <div>
+              Day income
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
