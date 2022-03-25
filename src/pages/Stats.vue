d64c33b28dec3c7ce35501b1ad8ea47103f94bb2
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-25 21:57:24 +0300

    col-auto does not let it to spread.

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page padding>
     <div class="row items-start q-gutter-md">
-      <div class="col">
+      <div class="col-auto">
         <total-purchase-and-price-card
           :total-purchase="3000000"
           :total-price="4500000"
         />
       </div>
-      <div class="col">
+      <div class="col-auto">
         <income-card
           :total-income="1500000"
           :day-income="250000"
