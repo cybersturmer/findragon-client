134d3dcc43071ce11e3e99e76d3fdabe3259b270
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-26 01:22:21 +0300

    Income triangles moved.

--- src/components/dash/stats/IncomeCard.vue
+++ src/components/dash/stats/IncomeCard.vue
@@ -1,31 +1,35 @@
 <template>
   <q-card bordered>
-    <q-card-section class="dash_half_thin_widget">
-      <div class="row items-end">
+    <q-card-section class="dash_half_thin_widget q-px-xl">
+      <div class="row items-center">
+        <div class="col text-subtitle1 text-bold">
+          Total Income:
+        </div>
         <div class="col-auto">
           <q-icon
             :name="totalIncomeIconParams.icon"
             :color="totalIncomeIconParams.color"
-            size="md"
+            size="sm"
           />
         </div>
-        <div class="col text-subtitle1 text-bold">
-          Total Income:
+        <div class="col">
           <span :class="`text-${totalIncomeIconParams.color}`">
             {{totalIncomeFormattedString}}
           </span>
         </div>
       </div>
       <div class="row items-center">
+        <div class="col">
+          Day Income:
+        </div>
         <div class="col-auto">
           <q-icon
             :name="dayIncomeIconParams.icon"
             :color="dayIncomeIconParams.color"
-            size="md"
+            size="sm"
           />
         </div>
-        <div class="col text-subtitle2">
-          Day Income:
+        <div class="col text-caption">
           <span :class="`text-${dayIncomeIconParams.color}`">
             {{dayIncomeFormattedString}}
           </span>
