11ec0963fe4b115f7d5a3bc91e20305ed258ccd1
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-25 20:25:59 +0300

    Better view for currencies.

--- src/components/dash/stats/TotalPurchaseAndPriceCard.vue
+++ src/components/dash/stats/TotalPurchaseAndPriceCard.vue
@@ -1,17 +1,35 @@
 <template>
-  <q-card>
-    <q-card-section>
-      <div class="text-subtitle1 text-bold">
-        Total Price: {{totalPrice}}
-      </div>
-      <div class="text-subtitle2">
-        Total Purchase: {{totalPurchase}}
+  <q-card bordered>
+    <q-card-section class="dash_half_thin_widget">
+      <div class="row items-end q-pa-sm">
+        <div class="col">
+          <div class="row">
+            <div class="col-auto q-pr-md">
+              <q-icon
+                name="account_balance"
+                size="52px" />
+            </div>
+            <div class="col text-h5">
+              <div>
+                {{ formattedTotalPrice }}
+              </div>
+              <div class="col-auto text-caption text-blue-grey-4">
+                Purchase: {{formattedTotalPurchase}}
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     </q-card-section>
   </q-card>
 </template>
 
+
+
 <script>
+import { computed } from 'vue'
+
 export default {
   name: 'TotalPurchaseAndPriceCard',
   props: {
@@ -22,8 +40,27 @@ export default {
       required: true
     }
   },
-  setup () {
-    return {}
+  setup (props) {
+    const formattedTotalPurchase = computed(() => {
+      return Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUR' // @todo change it to actual currency
+      })
+      .format(props.totalPurchase)
+    })
+
+    const formattedTotalPrice = computed(() => {
+      return Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUR' // @todo change it to actual currency
+      })
+      .format(props.totalPrice)
+    })
+
+    return {
+      formattedTotalPrice,
+      formattedTotalPurchase
+    }
   }
 }
 </script>
