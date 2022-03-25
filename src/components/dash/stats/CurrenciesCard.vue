bc51de17a430d1f0bbf7c31ca14f5cb3403b8a34
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-26 01:21:39 +0300

    Currency POC version.

--- src/components/dash/stats/CurrenciesCard.vue
+++ src/components/dash/stats/CurrenciesCard.vue
@@ -1,11 +1,51 @@
 <template>
   <q-card bordered>
-    <q-card-section class="dash_half_thin_widget">
+    <q-card-section
+      class="dash_half_thin_widget">
+      <div v-for="item in example" class="row items-center">
+        <div class="col text-subtitle2 text-bold">
+          {{ item.fromCurrency }}/{{ item.toCurrency }}
+        </div>
+        <div class="col-auto q-pl-md q-pr-none">
+          <q-icon
+            :name="item.dayGainPercent > 0 ? positiveIconAttrs.icon : negativeIconAttrs.icon"
+            :color="item.dayGainPercent > 0 ? positiveIconAttrs.color : negativeIconAttrs.color"
+            size="30px"
+          />
+        </div>
+        <div class="col-auto text-subtitle1 q-pr-md">
+          {{ item.price }}
+        </div>
+        <div :class="`col-auto text-caption text-${item.dayGainPercent > 0 ? positiveIconAttrs.color : negativeIconAttrs.color }`">
+          {{ item.dayGainPercent }}%
+        </div>
+      </div>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
+import { positiveIconAttrs, negativeIconAttrs } from './presets'
+
+const example = [
+  {
+    "ticker": "USDRUB",
+    "fromCurrency": "USD",
+    "toCurrency": "RUB",
+    "price": 101.25,
+    "previousClose": 96.715,
+    "dayGainPercent": -4.69
+  },
+  {
+    "ticker": "EURRUB",
+    "fromCurrency": "EUR",
+    "toCurrency": "RUB",
+    "price": 111.31,
+    "dayGain": 4.845249672072675,
+    "dayGainPercent": 4.55
+  }
+]
+
 export default {
   name: 'CurrenciesCard',
   props: {
@@ -16,7 +56,11 @@ export default {
     }
   },
   setup () {
-    return {}
+    return {
+      example,
+      positiveIconAttrs,
+      negativeIconAttrs
+    }
   }
 }
 </script>
