aa7db738b76f5769933fd98c42ecf4f99609c4b1
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-26 00:03:15 +0300

    Actually we've just added  this card.

--- src/components/dash/stats/CurrenciesCard.vue
+++ src/components/dash/stats/CurrenciesCard.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>My component</div>
+  <q-card bordered>
+    <q-card-section class="dash_half_thin_widget">
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
 export default {
   name: 'CurrenciesCard',
+  props: {
+    currencyRates: {
+      type: Array,
+      required: false,
+      default: []
+    }
+  },
   setup () {
     return {}
   }
