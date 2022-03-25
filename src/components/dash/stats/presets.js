87ab4ab666018cc340810eb250f1c955fab062e6
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-25 20:15:51 +0300

    Preset to simplify highlighting income up / down scenarios.

---
+++ src/components/dash/stats/presets.js
@@ -0,0 +1,11 @@
+export const negativeIconAttrs = {
+  icon: 'arrow_drop_down',
+  color: 'negative',
+  prefix: ''
+}
+
+export const positiveIconAttrs = {
+  icon: 'arrow_drop_up',
+  color: 'positive',
+  prefix: '+'
+}
