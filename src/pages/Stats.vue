b8da67f3bf44739feeb654b2e8a3238f90734ed2
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-03-31 01:06:36 +0300

    Just better sizing for middle block.

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,19 +1,19 @@
 <template>
   <q-page padding>
     <div class="row items-start q-gutter-md">
-      <div class="col-auto">
+      <div class="col">
         <total-purchase-and-price-card
           :total-purchase="3162948.23"
           :total-price="4587397.78"
         />
       </div>
-      <div class="col-grow">
+      <div class="col">
         <income-card
           :total-income="1424449.55"
           :day-income="254938.11"
         />
       </div>
-      <div class="col-auto">
+      <div class="col">
         <currencies-card
           :currency-rates="[]"
         />
