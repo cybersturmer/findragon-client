8b5ad89cca44c7e9e594a4e6e08f82b2a46a8114
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-01 02:27:57 +0300

    Icons.

--- src/components/dash/stats/TopLosersToday.vue
+++ src/components/dash/stats/TopLosersToday.vue
@@ -1,7 +1,15 @@
 <template>
   <q-card bordered>
-    <q-card-section>
-      <div class="text-h6">TOP Losers</div>
+    <q-card-section class="row items-stretch justify-between">
+      <div class="col text-h6">TOP Losers</div>
+      <div class="col-auto">
+        <q-btn
+          flat
+          round
+          icon="percent"
+          size="sm"
+        />
+      </div>
     </q-card-section>
     <q-separator inset />
     <q-card-section>
