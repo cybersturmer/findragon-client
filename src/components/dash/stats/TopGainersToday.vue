f30cee83df0957e635c6867527fa9e6494a9a568
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-01 02:07:44 +0300

    Skeleton for Top gainers / losers

--- src/components/dash/stats/TopGainersToday.vue
+++ src/components/dash/stats/TopGainersToday.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>My component</div>
+  <q-card bordered>
+    <q-card-section>
+      <div class="text-h6">TOP Gainers</div>
+    </q-card-section>
+    <q-separator inset />
+    <q-card-section>
+      Some content
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
 export default {
-  // name: 'ComponentName',
+  name: 'TopGainersToday',
   setup () {
     return {}
   }
