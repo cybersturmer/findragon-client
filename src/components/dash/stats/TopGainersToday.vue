74dd0e3770f7f719f82fe9d8c24cfd694b31acad
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-01 02:27:51 +0300

    Icons.

--- src/components/dash/stats/TopGainersToday.vue
+++ src/components/dash/stats/TopGainersToday.vue
@@ -1,7 +1,15 @@
 <template>
   <q-card bordered>
-    <q-card-section>
-      <div class="text-h6">TOP Gainers</div>
+    <q-card-section class="row items-stretch justify-between">
+      <div class="col text-h6">TOP Gainers</div>
+      <div class="col-auto">
+        <q-btn
+          flat
+          round
+          icon="paid"
+          size="sm"
+        />
+      </div>
     </q-card-section>
     <q-separator inset />
     <q-card-section>
