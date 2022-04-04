b979f838dba689fa08d6a839aeddaacdf1d3236f
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-04 03:36:54 +0300

    Mutation to init stats module state.

--- src/store/stats/mutations.js
+++ src/store/stats/mutations.js
@@ -1,4 +1,30 @@
-/*
-export function someMutation (state) {
+import {
+  removeElementById,
+  updateElementById,
+  StorageUtil
+} from 'src/services/utils'
+
+import { empty } from 'src/store/allocation/presets'
+
+const storageUtil = new StorageUtil(
+  'stats',
+  empty()
+)
+
+const saveAssetsState = (state) => {
+  storageUtil.setState('assets', state.assets)
 }
-*/
+
+export function ACTIVATE (state) {
+  state.enabled = true
+  storageUtil.setState('enabled', true)
+}
+
+export function SET_ASSETS (state, payload) {
+  state.assets = payload
+  saveAssetsState(state)
+}
+
+export function RESET (state) {
+  storageUtil.clearState(state)
+}
