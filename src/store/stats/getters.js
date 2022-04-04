502aca7a1155fc101543da24da3297d1f448151c
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-04 03:35:41 +0300

    Getters for stats module.

--- src/store/stats/getters.js
+++ src/store/stats/getters.js
@@ -1,4 +1,35 @@
-/*
-export function someGetter (state) {
+export function ASSETS (state) {
+  return state.assets
 }
-*/
+
+export function ASSET_BY_ID (state) {
+  return assetId => {
+    return state.assets
+      .filter((asset) => asset.id === assetId)[0]
+  }
+}
+
+export function ASSETS_GAINERS (state) {
+  return state.assets.filter((asset) => asset.change > 0)
+}
+
+export function ASSETS_LOSERS (state) {
+  return state.assets.filter((asset) => asset.change < 0)
+}
+
+
+export function ASSETS_TOP_GAINERS (state, getters) {
+  return maxAmount => {
+    return getters.ASSETS_GAINERS
+      .sort((a, b) =>  a.change - b.change )
+      .slice(0, maxAmount)
+  }
+}
+
+export function ASSETS_TOP_LOSERS (state, getters) {
+  return maxAmount => {
+    return getters.ASSETS_LOSERS
+      .sort((b, a) => a.change - b.change)
+      .slice(0, maxAmount)
+  }
+}
