9d93708a53eceaec7554f36a2c829ab698bc1b6b
Vladimir Shturmer <vladimir_shturmer@epam.com>
2022-04-09 23:24:21 +0300

    One more step to show losers and gainers better.

--- src/components/dash/stats/TopLosersToday.vue
+++ src/components/dash/stats/TopLosersToday.vue
@@ -14,9 +14,19 @@
     </q-card-section>
     <q-separator inset />
     <q-card-section>
-      <div v-for="asset in top">
-        {{ asset.code }} {{ asset.change }} {{ asset.change_p }}%
-      </div>
+      <q-list dense>
+        <q-item v-for="asset in top">
+          <q-item-section side>
+            {{ asset.code }}
+          </q-item-section>
+          <q-item-section v-if="!isChangeInPercentage">
+            {{ asset.portfolio_change }}
+          </q-item-section>
+          <q-item-section v-else>
+            {{ asset.portfolio_change_p }}%
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-card-section>
   </q-card>
 </template>
